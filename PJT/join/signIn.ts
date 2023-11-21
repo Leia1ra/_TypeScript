1a850e28e6b5f7ef81b0a09fe0c23e4220a2bdcd
Leia1ra <leiaira92@gmail.com>
2023-11-21 20:50:29 +0900

    11월 21일

--- PJT/join/signIn.ts
+++ PJT/join/signIn.ts
@@ -2,28 +2,34 @@ enum errType {
     unfilled = '미입력된 양식이 존재합니다.',
     existID = '존재하는 아이디 입니다.',
     pwCheck = '입력된 비밀번호와 확인번호가 일치하지 않습니다.',
-    capsLock = 'CapsLock이 켜져있습니다.'
+    pwStrength = '6-15자리, 특수문자, 대소문자, 숫자를 최소 2가지를 조합하여 비밀번호를 입력하세요',
+    capsLock = 'CapsLock이 켜져있습니다.',
+
 }
 enum condition {
     default='1px solid black',
     err = '2px solid red',
     focus = '2px solid #1ebee6'
 }
-function validationCheck(element:HTMLInputElement, reg:RegExp, errMsgbox?:HTMLDivElement, errMsg?:string ) {
+function validationCheck(element:HTMLInputElement, reg:RegExp, errMsgbox?:HTMLDivElement, errMsg?:string ) : boolean {
+    let result : boolean = false;
     element.addEventListener("focusout", function () {
         if(!reg.test(this.value)){
             this.style.border = condition.err;
             errMsgbox.innerHTML = `올바른 ${errMsg} 양식이 아닙니다.`;
             errMsgbox.style.display = 'block';
+            result = false;
         } else {
             this.style.border = condition.default;
             errMsgbox.innerHTML = '';
             errMsgbox.style.display = 'none';
+            result = true;
         }
     })
     element.addEventListener("focusin", function () {
         this.style.border = condition.focus;
     })
+    return result;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -59,10 +65,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     /*유효성검사*/
     let idReg = /^[a-zA-Z]{1}[a-zA-Z0-9_]{6,12}$/;
-    validationCheck(id, idReg, errMsg,"아이디"); /*아이디 유효성 검사*/
+    let idPassable = validationCheck(id, idReg, errMsg,"아이디"); /*아이디 유효성 검사*/
 
     /*비밀번호*/
-    function pwStrengthCheck(pwIn:string) {
+    function pwStrengthCheck():boolean {
         let strength = 0;
         const pwReg:RegExp[] = [/* + 는 하나 이상 포함*/
             /[a-z]+/,
@@ -72,38 +78,89 @@ document.addEventListener('DOMContentLoaded', function () {
         ];
         pwReg.forEach((value:RegExp, idx:number) => {
             // strength += value.test(pw.value) ? 1 : 0
-            strength += pwIn.match(value) ? 1 : 0;
+            if(pw.value.length < 6){
+                meter.style.setProperty("--progress", "red");
+                strength = 1;
+                // meter.style.backgroundColor = 'red';
+            } else if(pw.value.length > 15) {
+                meter.style.setProperty("--progress", "red");
+                strength = 1;
+                // meter.style.backgroundColor = 'red';
+            } else {
+                meter.style.backgroundColor = 'white';
+                strength += value.test(pw.value) ? 1 : 0
+            }
         })
         meter.value = strength;
         switch (strength) {
             case 1 :
-                meter.style.backgroundColor = 'red'
+                // meter.style.backgroundColor = 'red'
+                meter.style.setProperty("--progress", "red");
                 break
             case 2:
             case 3:
-                meter.style.backgroundColor = 'orange'
+                // meter.style.backgroundColor = 'orange'
+                meter.style.setProperty("--progress", "orange");
                 break
             case 4:
-                meter.style.backgroundColor = 'green'
+                // meter.style.backgroundColor = 'green'
+                meter.style.setProperty("--progress", "green");
                 break
         }
+        if(strength >= 2 ) { return true }
+        else { return false }
     }
-
-    pw.addEventListener('keyup', function (event) {
-        pwStrengthCheck(pw.value);
-        // console.log('ㅎㅇ');
+    function pwCheck(passable:boolean) {
+        if(passable == undefined || !passable){
+            errMsg.innerHTML = errType.pwStrength;
+            errMsg.style.display = 'block';
+        } else if(passable == undefined){
+            pw.style.border = condition.focus;
+        } else if(!passable){
+            pw.style.border = condition.err;
+        } else {
+            errMsg.innerHTML = "";
+            errMsg.style.display = 'none';
+            pw.style.border = condition.focus;
+        }
+    }
+    let errBuffer : number = 0;
+    let passable : boolean ;
+    pw.addEventListener('focusin', function () {
+        errBuffer = errCnt;
+        console.log(`pass : ${passable} | err : ${errCnt} | buf : ${errBuffer}`);
+        pw.addEventListener('keyup', function () {
+            passable = pwStrengthCheck();
+            console.log(passable);
+            if(passable){
+                pw.style.border = condition.focus;
+            } else {
+                pw.style.border = condition.err;
+            }
+        })
+        pwCheck(passable)
+    })
+    pw.addEventListener('focusout', function () {
+        pwCheck(passable);
+        if(!passable){ ++errCnt; }
+        else {
+            pw.style.border = condition.default;
+            errCnt -= errBuffer;
+            errBuffer=0;
+            console.log(`pass : ${passable} | err : ${errCnt} | buf : ${errBuffer}`);
+        }
     })
 
-    let nameReg = /^[가-힣]{2,7}$/;
-    validationCheck(name, nameReg, errMsg, "이름");/*이름 유효성 검사*/
-    let emailReg = /^\w{4,14}[@][a-z]{3,10}[.][a-z]{2,3}([.][a-z]{2,3})?$/;
-    validationCheck(email, emailReg, errMsg, "이메일");/*이메일 유효성 검사*/
 
+    let nameReg = /^[가-힣]{2,7}$/;
+    let namePassable = validationCheck(name, nameReg, errMsg, "이름");/*이름 유효성 검사*/
+    let emailReg = /^\w{4,14}[@][a-z]{3,10}[.][a-z]{2,3}([.][a-z]{2,3})?$/;
+    let emailPassable = validationCheck(email, emailReg, errMsg, "이메일");/*이메일 유효성 검사*/
 
 
     /* 사업자 */
     let business = document.getElementById('business') as HTMLInputElement;
-    business.addEventListener('change', function (event) {
+    business.addEventListener('change', function () {
         let inputBox = document.getElementById('business-input');
         if(business.checked == true){
             document.getElementById('business-input').innerHTML = '비즈니스 사용자임';
@@ -119,12 +176,17 @@ document.addEventListener('DOMContentLoaded', function () {
         if(id.value === ""){
             id.style.border = condition.err;
             ++errCnt
+        } else if(!idPassable){
+            ++errCnt
         }
 
         /*PW*/
         if(pw.value === ""){
             pw.style.border = condition.err;
             ++errCnt
+        } else if (!passable){
+            pw.style.border = condition.err;
+            errMsg.innerHTML = errType.pwStrength;
         }
         if(pwRe.value === ""){
             pwRe.style.border = condition.err;
@@ -142,11 +204,15 @@ document.addEventListener('DOMContentLoaded', function () {
         if(name.value == ""){
             name.style.border = condition.err;
             ++errCnt;
+        } else if(!namePassable) {
+            ++errCnt;
         }
 
         /*Email*/
         if(email.value == ""){
             email.style.border = condition.err;
+            ++errCnt;
+        } else if(!emailPassable){
             ++errCnt;
         }
 
